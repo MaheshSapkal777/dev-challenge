be379f16dc8bd518456a79edf0c40f464de8ecbe
MaheshSapkal777 <60068756+MaheshSapkal777@users.noreply.github.com>
2020-01-21 13:19:06 +0530

    Add files via upload
    Fine tunning naming conventions

--- es6/Home.js
+++ es6/Home.js
@@ -1,29 +1,56 @@
 const url = "ws://localhost:8011/stomp"
 const priceUrl = "/fx/prices"
-let currencyDataArray = [];
-let midpriceArray = [];
 const client = Stomp.client(url)
-let tableHeaderPresent = false;
+let currencyDataArray = [];
+
 client.debug = function (msg) {
   if (global.DEBUG) {
     console.info(msg)
   }
 }
+
 function connectCallback(e) {
   console.log(e)
-  /** to get currency data from socket */
-  getCurrencyData();
+  subscribePriceList(); /** get currency data from socket */
 }
 
 client.connect({}, connectCallback, function (error) {
   alert(error.headers.message)
 })
 
-function getCurrencyData() {
-  client.subscribe(priceUrl, priceListResponse);
+function subscribePriceList() {
+  client.subscribe(priceUrl, onPriceListResponse);
 }
-/** function to render table header */
-function createTableHeader() {
+
+/** callback function for price data response */
+function onPriceListResponse(response) {
+  const data = response.body
+  if (data) {
+    const priceData = JSON.parse(data);
+    const { bestBid, bestAsk } = priceData;
+    const midprice = (bestBid + bestAsk) / 2
+    const isTableCreated = createTableWithHeaders();
+    if (isTableCreated) {
+      const index = currencyDataArray.findIndex(currency => currency.name == priceData.name) //check if currency already exist in table.
+      if (index >= 0) {
+        /**update existing currency data */
+        updateExistingCurrencyDataInArray(index, midprice, priceData);
+      } else {
+        /**create new entry for currency data */
+        addNewCurrencyDataInArray(midprice, priceData);
+      }
+      /** sort currency data by best last changed bid */
+      currencyDataArray.sort(function (a, b) {
+        return a.lastChangeBid - b.lastChangeBid;
+      });
+      /**render data to table view */
+      renderTableData();
+    }
+  }
+}
+
+/** function to render empty table with headers */
+function createTableWithHeaders() {
   const newTable = '<table id="bid-table"></table>'
   document.getElementById('root').innerHTML = newTable
   const tableHeaders = ['Name', 'Best Bid', 'Best Ask', 'Open Bid', 'Open Ask', 'Last Change Ask', 'Last Change Bid', 'Midprice']
@@ -37,58 +64,32 @@ function createTableHeader() {
   })
   return true
 }
-/** callback function for price data  */
-function priceListResponse(message) {
-  const data = message.body
-  if (data) {
-    const jsonData = JSON.parse(data);
-    const { bestBid, bestAsk } = jsonData;
-    const midprice = (bestBid + bestAsk) / 2
-    const tableHeaderCreated = createTableHeader();
-    if (tableHeaderCreated) {
-      const index = currencyDataArray.findIndex(currency => currency.name == jsonData.name) //check if currency already exist in table.
-      if (index >= 0) {
-        /**update existing currency data */
-        updateExitingCurrencyData(index, midprice, jsonData);
-      } else {
-        /**create new entry for currency data */
-        creatNewCurrencyData(midprice, jsonData);
-      }
-      /** sort currency data by best last changed bid */
-      currencyDataArray.sort(function (a, b) {
-        return a.lastChangeBid - b.lastChangeBid;
-      });
-      /**render data to table view */
-      renderTableData();
-    }
-  }
-}
 
 /**create new entry for those currecy data, which is not present in table */
-function creatNewCurrencyData(midprice, jsonData) {
+function addNewCurrencyDataInArray(midprice, priceData) {
   let newMidPriceArray = []
   newMidPriceArray.push(midprice)
-  jsonData.midprice = newMidPriceArray
-  jsonData.lastMidPrice = midprice;
-  currencyDataArray.push(jsonData)
+  priceData.midprice = newMidPriceArray
+  priceData.lastMidPrice = midprice;
+  currencyDataArray.push(priceData)
 }
 
 /** update new price data for existing currency */
-function updateExitingCurrencyData(index, midprice, jsonData) {
+function updateExistingCurrencyDataInArray(index, midprice, priceData) {
   let currentMidPriceArray = currencyDataArray[index].midprice
   /**check if midprice data is recent 30 sec. */
   if (currentMidPriceArray.length >= 30) {
     currentMidPriceArray.shift() /**remove first sec. value to update new one */
   }
   currentMidPriceArray.push(midprice)
-  jsonData.midprice = currentMidPriceArray;
-  jsonData.lastMidPrice = midprice;
-  currencyDataArray[index] = jsonData;
-  updateOtherCurrencyMidprice(index);
+  priceData.midprice = currentMidPriceArray;
+  priceData.lastMidPrice = midprice;
+  currencyDataArray[index] = priceData;
+  updateMidPriceForRemainingCurrencies(index);
 }
 
-/**update other currency midprice to last one. */
-function updateOtherCurrencyMidprice(index) {
+/**update remaining currency midprice to last one. */
+function updateMidPriceForRemainingCurrencies(index) {
   currencyDataArray.map((data, idx) => {
     if (idx !== index) {
       /**check if midprice data is recent 30 sec. */
@@ -102,7 +103,8 @@ function updateOtherCurrencyMidprice(index) {
     }
   })
 }
-/** create table data view for currency data */
+
+/** insert currency data into table view from currency array. */
 function renderTableData() {
   currencyDataArray.map((item, index) => {
     const table = document.getElementById("bid-table");
